0af9006f987e5d98d32245e9ccf8ca4430026930
nbaak <niklas.baak@outlook.com>
2023-03-11 17:35:02 +0100

    added gui for queue

--- src/App.py
+++ src/App.py
@@ -14,47 +14,66 @@ def draw_grid(screen, blocksize=20, game:Game=None):
         for y in range(len(game.field)):
             for x in range(len(game.field[y])):
                 rect = pygame.Rect(x * blocksize, y * blocksize, blocksize, blocksize)
-                pos_value = game.get_at((x, y)) 
+                pos_value = game.get_at((x, y))
                 if pos_value >= 0:
                     pygame.draw.rect(screen, colors.WHITE, rect, 0)  # fill
 
                 else:
-                    pygame.draw.rect(screen, colors.BLACK, rect, 0)  # fill
+                    pygame.draw.rect(screen, colors.GRAY, rect, 0)  # fill
 
-                pygame.draw.rect(screen, colors.GRAY, rect, 1)  # Border
-                
+                pygame.draw.rect(screen, colors.BLACK, rect, 1)  # Border
+
                 if pos_value >= 0:
-                    screen.blit(font.render(str(pos_value), True, colors.BLACK), (x*blocksize, y*blocksize))
-                
+                    screen.blit(font.render(str(pos_value), True, colors.BLACK), (x * blocksize, y * blocksize))
+
     else:
         exit()
 
 
-def draw_queue(screen, blocksize=20, game:Game=None):
+def draw_queue(screen, blocksize=20, start_at_height=0, game:Game=None):
     if game:
-        ...
+        queue = game.get_queue()
+        font = pygame.font.SysFont('Arial', blocksize)
+        y = start_at_height
+        for index, element in enumerate(queue):
+            x = index * blocksize
+            rect = pygame.Rect(x, y, blocksize, blocksize)
+            if index == 0:
+                pygame.draw.rect(screen, colors.WHITE, rect, 0)  # fill
+            else:
+                pygame.draw.rect(screen, colors.GRAY, rect, 0)  # fill
+            pygame.draw.rect(screen, colors.BLACK, rect, 1)  # Border
+            screen.blit(font.render(str(element), True, colors.BLACK), (x, y))
 
-def set_value(value:int, pos:tuple, game:Game):
+
+def set_value(pos:tuple, game:Game):
     total_value = 0
-    if value and tuple:
-        game.place_at(value, pos)
-        # game.show()
-        pts = game.check_rules(pos, value)
-        # print(f'you earned {pts} points')
-        total_value += pts
-        while pts > 0:
+    if game:
+        value = game.queue[0]
+        placed = game.place_at(value, pos)
+        
+        if placed == value:
+            game.take_next_queue_element()
             pts = game.check_rules(pos, value)
             total_value += pts
             
+            while pts > 0:
+                pts = game.check_rules(pos, value)
+                total_value += pts
+
     return total_value
 
 
 def main(width=4, height=3):
-    game = Game(width, height, 1)
+    # TODO: level Loop
+    game = Game(width, height, 0)
     pygame.init()
     pygame.display.set_caption('Block Riddle')
     dims = game.dims()
-    screen = pygame.display.set_mode((dims[0] * BLOCKSIZE, dims[1] * BLOCKSIZE + BLOCKSIZE))
+    window_width = max([dims[0] * BLOCKSIZE, (BLOCKSIZE//2) * len(game.get_queue())])
+    window_height = dims[1] * BLOCKSIZE + int(.9 * BLOCKSIZE)
+    
+    screen = pygame.display.set_mode((window_width, window_height))
     clock = pygame.time.Clock()
     screen.fill(colors.WHITE)
 
@@ -64,25 +83,25 @@ def main(width=4, height=3):
 
     while True:
         draw_grid(screen, blocksize=BLOCKSIZE, game=game)
+        draw_queue(screen, blocksize=BLOCKSIZE // 2, start_at_height=window_height - BLOCKSIZE // 2, game=game)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-                
+
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 x, y = pos
                 x, y = x // BLOCKSIZE, y // BLOCKSIZE
                 pos = (x, y)
-        
+
         if pos:
-            points += set_value(1, pos, game)
+            points += set_value(pos, game)
             pygame.display.set_caption(f'Block Riddle {points}')
             pos = None
-            
-            
+
         pygame.display.update()
 
 
 if __name__ == "__main__":
-    main(4, 3)
+    main(5, 5)
