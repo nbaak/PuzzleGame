53859aab3a30b617c03b081d0e40a2b4e7ca8c1e
nbaak <niklas.baak@outlook.com>
2023-03-12 12:20:52 +0100

    added a screenshot button and a status bar

--- Readme.md
+++ Readme.md
@@ -1,5 +1,7 @@
+<img src="example.jpg" />
+
 # Block Add Game
-Add Blocks together.
+A small Puzzle Game written in Python using Pygame.
 
 ## How to play
 You get a block from the queue and you have to place it on the field. If the block gets grouped with 3 or more touching Blocks of the same type, the will be merged to a Block of a higher tier.
--- src/App.py
+++ src/App.py
@@ -2,6 +2,9 @@ from Game import Game
 import colors
 import pygame
 import sys
+import os
+import uuid
+import py
 
 WINDOW_HEIGHT = 400
 WINDOW_WIDTH = 400
@@ -10,7 +13,7 @@ BLOCKSIZE = 200
 
 def draw_grid(screen, blocksize=20, game:Game=None):
     if game:
-        font = pygame.font.SysFont('Arial', blocksize)
+        font = pygame.font.SysFont('Arial', int(blocksize * .8))
         for y in range(len(game.field)):
             for x in range(len(game.field[y])):
                 rect = pygame.Rect(x * blocksize, y * blocksize, blocksize, blocksize)
@@ -33,7 +36,7 @@ def draw_grid(screen, blocksize=20, game:Game=None):
 def draw_queue(screen, blocksize=20, start_at_height=0, game:Game=None):
     if game:
         queue = game.get_queue()
-        font = pygame.font.SysFont('Arial', blocksize)
+        font = pygame.font.SysFont('Arial', int(blocksize * .8))
         y = start_at_height
         for index, element in enumerate(queue):
             x = index * blocksize
@@ -46,20 +49,30 @@ def draw_queue(screen, blocksize=20, start_at_height=0, game:Game=None):
             screen.blit(font.render(str(element), True, colors.BLACK), (x, y))
 
 
+def draw_status(screen, points:int=0, level:int=0, fontsize=20, start_at_height:int=0, width=20, game:Game=None):
+    fontsize = int(fontsize * .8)
+    status_text = f'Level: {level} Points: {points}'
+    font = pygame.font.SysFont('Arial', fontsize)
+    
+    rect = pygame.Rect(0, start_at_height, width, fontsize)
+
+    pygame.draw.rect(screen, colors.WHITE, rect, 0)  # background
+    
+    screen.blit(font.render(status_text, True, colors.BLACK), (5, start_at_height))
+
 def set_value(pos:tuple, game:Game):
     total_value = 0
     gameover = False
-    
+
     if game:
         value = game.queue[0]
         placed = game.place_at(value, pos)
-        
-        
+
         if placed == value:
             pts, gameover = game.check_rules(pos, value)
             total_value += pts
             game.take_next_queue_element()
-            
+
             while pts > 0:
                 pts, gameover = game.check_rules(pos, value)
                 total_value += pts
@@ -67,15 +80,15 @@ def set_value(pos:tuple, game:Game):
     return total_value, gameover
 
 
-def main(width=4, height=3):
+def main(width=4, height=3, level=0):
     # TODO: level Loop
-    game = Game(width, height, 0)
+    game = Game(width, height, level)
     pygame.init()
     pygame.display.set_caption('Block Riddle')
     dims = game.dims()
-    window_width = max([dims[0] * BLOCKSIZE, (BLOCKSIZE//2) * len(game.get_queue())])
+    window_width = max([dims[0] * BLOCKSIZE, (BLOCKSIZE // 2) * len(game.get_queue())])
     window_height = dims[1] * BLOCKSIZE + int(.9 * BLOCKSIZE)
-    
+
     screen = pygame.display.set_mode((window_width, window_height))
     clock = pygame.time.Clock()
     screen.fill(colors.WHITE)
@@ -88,6 +101,8 @@ def main(width=4, height=3):
     while True:
         draw_grid(screen, blocksize=BLOCKSIZE, game=game)
         draw_queue(screen, blocksize=BLOCKSIZE // 2, start_at_height=window_height - BLOCKSIZE // 2, game=game)
+        draw_status(screen, points, level, BLOCKSIZE // 2, start_at_height=window_height - int(BLOCKSIZE*.9), width=window_width, game=game)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -99,19 +114,32 @@ def main(width=4, height=3):
                 x, y = x // BLOCKSIZE, y // BLOCKSIZE
                 pos = (x, y)
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    if not os.path.exists('./screenshots'):
+                        os.makedirs('./screenshots', exist_ok=True)
+
+                    filename = str(uuid.uuid4())
+                    pygame.image.save(screen, f'./screenshots/{filename}.jpg')
+
+                if event.key == pygame.K_r:
+                    print('forced reset')
+                    game.reset()
+
         if pos:
             pts, gameover = set_value(pos, game)
             points += pts
             pygame.display.set_caption(f'Block Riddle {points}')
             pos = None
 
-        
         if gameover:
             game.reset()
             print("game over")
             gameover = False
+            points = 0
 
         pygame.display.update()
 
+
 if __name__ == "__main__":
-    main(2, 2)
+    main(5, 4, 0)
