58c7890e45014cb12542f2f031b0154190a1e1c5
Niklas Baak <niklas.baak@outlook.com>
2023-03-20 19:30:27 +0100

    added test module for pytorch

---
+++ src/sb3/test_torch.py
@@ -0,0 +1,11 @@
+
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print('Using device:', device)
+print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
